46b0a7c10b95e98c9582e72bb3b0c128233b9845
Kareen Tariman <kareentariman@gmail.com>
2026-06-28 21:15:59 +0800

    Wire survey to Apps Script backend endpoint

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste your deployed Google Apps Script Web App URL here (see README.md step 4).
 // It looks like: https://script.google.com/macros/s/AKfy.....XYZ/exec
-window.SURVEY_ENDPOINT = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+window.SURVEY_ENDPOINT = "https://script.google.com/macros/s/AKfycbzxQOUP0mzqFy6fP-ksw-z-B0oskLnK42d_b6LJgXqx4sbTx2xjuodDrkw9ucgqgW9sJQ/exec";
